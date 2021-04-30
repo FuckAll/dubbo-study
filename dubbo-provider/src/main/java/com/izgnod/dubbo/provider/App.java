8f0918e5210b1b665004006ab7791206926f1d51
FuckAll <wxdanobody2011>
2021-05-01 00:06:19 +0800

    增加配置

--- dubbo-provider/src/main/java/com/izgnod/dubbo/provider/App.java
+++ dubbo-provider/src/main/java/com/izgnod/dubbo/provider/App.java
@@ -1,19 +1,16 @@
 package com.izgnod.dubbo.provider;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author: izgnod.
  * @date: 2021/4/30
  * @description:
  */
+@SpringBootApplication
 public class App {
-    public static void main(String[] args) throws IOException {
-        //加载xml配置文件启动
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("provider.xml");
-        context.start();
-        System.in.read(); // 按任意键退出
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
--- dubbo-provider/src/main/java/com/izgnod/dubbo/provider/ProviderServiceImpl.java
+++ dubbo-provider/src/main/java/com/izgnod/dubbo/provider/ProviderServiceImpl.java
@@ -2,12 +2,14 @@ package com.izgnod.dubbo.provider;
 
 
 import com.izgnod.dubbo.api.ProviderService;
+import org.apache.dubbo.config.annotation.Service;
 
 /**
  * @author: izgnod.
  * @date: 2021/4/30
  * @description:
  */
+@Service
 public class ProviderServiceImpl implements ProviderService {
     @Override
     public String sayHello(String word) {
